f7d7b83608d3730522b8a9e0dee6e69b74f9be0a
ygjeong <ygjeong@kcp.co.kr>
2023-04-14 16:52:44 +0900

    first

---
+++ src/main/java/com/example/studyobjectify/StudyObjectifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyobjectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyObjectifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyObjectifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyobjectify/StudyObjectifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyobjectify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyObjectifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
